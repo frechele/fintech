92da3f9f9d9331428c73f1fa4fd10ee989fb8f48
Junyeong Park <jyp10987@gmail.com>
2022-12-12 02:21:27 +0900

    feat(client): create config

--- gofund/src/main.rs
+++ gofund/src/main.rs
@@ -1,15 +1,51 @@
+use serde_derive::Deserialize;
+use std::fs;
+use std::process::exit;
+use toml;
+
 use cursive::views::Dialog;
 
 fn main() {
+    let config = load_configuration();
+
     let mut siv = cursive::default();
 
     siv.set_window_title("Goindol Fund Client");
 
     siv.add_layer(
-        Dialog::text("Unimplemented")
-            .title("Information")
-            .button("OK", |s| s.quit()),
+        Dialog::text(format!(
+            "Server ip: {}\nServer port: {}",
+            &config.ip, &config.port
+        ))
+        .title("Information")
+        .button("OK", |s| s.quit()),
     );
 
     siv.run();
+}
+
+#[derive(Deserialize)]
+struct Config {
+    ip: String,
+    port: u16,
+}
+
+fn load_configuration() -> Config {
+    let contents = match fs::read_to_string("config.toml") {
+        Ok(c) => c,
+        Err(_) => {
+            eprintln!("Could not read config file");
+            exit(-1);
+        }
+    };
+
+    let data: Config = match toml::from_str(&contents) {
+        Ok(d) => d,
+        Err(_) => {
+            eprintln!("Unable to load config file");
+            exit(-1);
+        }
+    };
+
+    data
 }
