6ff7a3573ffa3031bb42bbbb77488eaebab82a0c
Junyeong Park <jyp10987@gmail.com>
2022-12-12 14:24:35 +0900

    Revert "feat(server): create analyzer module"
    This reverts commit 2ea56936b55642b69418e34cbfc956bb5f16170f.

--- gofund_srv/src/main.rs
+++ gofund_srv/src/main.rs
@@ -1,9 +1,5 @@
 #[macro_use] extern crate rocket;
 
-mod analyzer;
-
-use std::time::Duration;
-
 #[get("/")]
 fn index() -> &'static str {
     "Hell World!"
@@ -11,15 +7,10 @@ fn index() -> &'static str {
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
-    let analyzer = analyzer::setup_analyzer();
-    let thread_handle = analyzer.watch_thread(Duration::from_secs(1));
-
     let _rocket = rocket::build()
         .mount("/", routes![index])
         .launch()
         .await?;
-
-    thread_handle.stop();
 
     Ok(())
 }
